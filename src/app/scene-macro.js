dc419508cb0bdb5098de87decfe5b2a97a32945f
cliffhall <github@futurescale.com>
2019-10-08 19:42:13 -0400

    Got scene and branch macro rendering with action links that change the branch!!!

--- src/app/scene-macro.js
+++ src/app/scene-macro.js
@@ -21,6 +21,38 @@
 (function () {
     'use strict';
 
+    // Function to render the current branch
+    setup.renderCurrentBranch = () => {
+
+        // Bail if no current scene or branches
+        let currentScene = State.getVar('$currentScene');
+        if (!currentScene || !currentScene.branch || !currentScene.branches) return;
+
+        // Get the current branch text and actions
+        let branch = currentScene.branches[currentScene.branch];
+        let text = branch[0];
+        let actions = branch.slice(1);
+
+        // Empty the scene container
+        let container = $(State.getVar('$sceneContainer'));
+        container.empty();
+
+        // Add the branch text to the scene container
+        container.wiki(text);
+
+        // Add the branches actions (if any) to the scene container
+        if (actions.length) {
+            let list = $(document.createElement('ul'));
+            actions.forEach(action => {
+                let bullet = $(document.createElement('li'));
+                bullet
+                    .wiki(action)
+                    .appendTo(list);
+            });
+            list.appendTo(container);
+        }
+    };
+
     Macro.add('scene', {
         tags: null,
         handler : function () {
@@ -31,8 +63,11 @@
             }
 
             // Set the current scene to this one, and reset the current branch
-            State.setVar('$currentScene', this.args[0]);
-            State.setVar('$currentBranch', null);
+            State.setVar('$currentScene', {
+                scene: this.args[0],
+                branch: null,
+                branches: {}
+            });
 
             // Create scene container, store it, and add it to the DOM
             let container = document.createElement('div');
@@ -49,51 +84,44 @@
         tags: ['action'],
         handler : function () {
 
-            // Name of branch
-            let branchName;
+            let branchName, isCurrent = false;
+            let currentScene = State.getVar('$currentScene');
 
             // Throw error if branch not named
             if (this.args.length < 1 || typeof this.args[0] !== 'string' || !this.args[0].trim()) {
                 return this.error('You must name each branch');
+            } else {
+                branchName = this.args[0].trim();
             }
 
-            // Set the current branch to this one
-            if (!!State.getVar('$currentBranch') && this.args.length === 2 || this.args[1] === 'start') {
-                State.setVar('$currentBranch', this.args[0]);
+            // Set the current branch to this one if no branch is set
+            if (!currentScene.branch && this.args.length === 2 || this.args[1] === 'start') {
+                currentScene.branch = branchName;
+                isCurrent = true;
             }
+
+            // Parse branch contents
+            currentScene.branches[branchName] = this.payload.map(part => {
+                let retVal;
+                switch (part.name) {
+                    case 'branch':
+                        retVal = String(this.payload[0].contents).trim();
+                        break;
+                    case 'action':
+                        retVal = `<<link "${String(part.contents).trim()}">>\
+                        <<set $currentScene.branch to "${String(part.args[0]).trim()}">>\
+                        <<run setup.renderCurrentBranch()>>\
+                        <</link>>`;
+                        break;
+                }
+                return retVal;
+            });
 
             // Render branch if current
-            if (State.getVar('$currentBranch') === this.args[0]) {
-
-                // Branch actions
-                let contents = this.payload.map(part => {
-                    let retVal;
-                    switch (part.name) {
-                        case 'branch':
-                            retVal = String(this.payload[0].contents).trim();
-                            break;
-                        case 'action':
-                            retVal = `<<link "${String(part.contents).trim()}">><<set $currentBranch to "${part.args[0]}">><</link>>`;
-                            console.log(retVal);
-                            break;
-                    }
-                    return retVal;
-                });
-
-                let branch = contents.shift();
-                let container = $(State.getVar('$sceneContainer'));
-                container.wiki(branch);
-                if (contents.length) {
-                    let list = $(document.createElement('ul'));
-                    contents.forEach(action => {
-                        let bullet = $(document.createElement('li'));
-                        bullet
-                            .wiki(action)
-                            .appendTo(list);
-                    });
-                    list.appendTo(container);
-                }
+            if (isCurrent) {
+                setup.renderCurrentBranch();
             }
+
         }
     });
 
